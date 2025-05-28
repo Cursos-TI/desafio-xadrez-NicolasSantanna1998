da60db6d14260451c041a99020f5b7dc418aae46
NicolasSantanna1998 <nicolascaputto32@gmail.com>
2025-05-28 17:06:09 +0000

    xadrez.c

---
+++ xadrez1.c
@@ -0,0 +1,26 @@
+# include <stdio.h>
+
+int main(){
+
+
+    printf("BISPO\n");
+   
+
+    for(int i = 0; i < 5; i++){
+        printf("cima,direita\n");
+    }
+
+    printf("TORRE\n");
+
+    for(int i = 0; i < 5; i++){
+        printf("direita\n");
+    }
+
+    printf("RAINHA\n");
+
+    for(int i = 0; i < 8; i++){
+        printf("esquerda\n");
+    }
+
+   
+}
